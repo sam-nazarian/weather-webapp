3e52158162292105bbca8dff46672e173f3bc65f
samanfathnazarian <saman.nazarian17@gmail.com>
2022-10-18 20:47:36 -0500

    added min & max info to current weather also added a 5-day forecast

--- src/js/views/renderView.js
+++ src/js/views/renderView.js
@@ -9,18 +9,31 @@ const dateDom = document.querySelector('.date');
 const weatherDescriptionDom = document.querySelector('.weather-description');
 const iconDom = document.querySelector('.icon');
 const tempDom = document.querySelector('.temp');
-const feelsLikeDom = document.querySelector('.feels-like');
+const tempHighTextDom = document.querySelector('.temp-high-text');
+const tempLowTextDom = document.querySelector('.temp-low-text');
+
 const feelsLikeTempDom = document.querySelector('.feels-like-temp');
 const humidityValueDom = document.querySelector('.humidity-value');
 const windSpeedValueDom = document.querySelector('.wind-speed-value');
-const visibilityValueDom = document.querySelector('.visibility-value');
+
 const sunriseValueDom = document.querySelector('.sunrise-value');
+const sunsetValueDom = document.querySelector('.sunset-value');
+const sunriseUnitDom = document.querySelector('.sunrise-unit');
+const sunsetUnitDom = document.querySelector('.sunset-unit');
 
 // const hourlyCard = document.querySelectorAll('.hourly-card');
 const hourlyTimeDom = Array.from(document.querySelectorAll('.hourly-time'));
 const hourlyImgDom = Array.from(document.querySelectorAll('.hourly-img'));
 const hourlyTempDom = Array.from(document.querySelectorAll('.hourly-temp'));
 
+//5-DAY-FORECAST
+const fiveDayForecastDayDom = document.querySelectorAll('.five-day-forecast-day');
+const fiveDayForecastImgDom = document.querySelectorAll('.five-day-forecast-img');
+
+const fiveDayForecastTempHigh = document.querySelectorAll('.five-day-forecast-temp-high');
+const fiveDayForecastTempLow = document.querySelectorAll('.five-day-forecast-temp-low');
+
+//////////////////////////
 function getHours(internationalTime) {
   let hours = internationalTime.hours();
   let minutes = internationalTime.minutes();
@@ -48,6 +61,13 @@ function formatTodaysDate(date, lat, lng) {
   return str;
 }
 
+function formatWeekDayName(date, lat, lng) {
+  const internationalTime = ts.getFuzzyLocalTimeFromPoint(date, [lng, lat]);
+  const [day] = internationalTime.toString().split(' ');
+
+  return day;
+}
+
 export async function render(fetchFiveDayForecast, lat, lng, metric) {
   //fetch for data
   const data = await fetchFiveDayForecast(lat, lng);
@@ -59,12 +79,23 @@ export async function render(fetchFiveDayForecast, lat, lng, metric) {
   countryNameDom.textContent = `${byIso(data.city.country).country} ${countryCodeEmoji(data.city.country)}`;
   weatherDescriptionDom.textContent = data.list[0].weather[0].description;
   tempDom.textContent = `${Math.round(data.list[0].main.temp)}°C`;
+
+  const [maxTemp, minTemp] = findMinAndMaxTemp(0, data);
+  tempHighTextDom.textContent = maxTemp;
+  tempLowTextDom.textContent = minTemp;
+
   feelsLikeTempDom.textContent = `${Math.round(data.list[0].main.feels_like)}°C`;
   iconDom.src = `src/img/icons/${data.list[0].weather[0].icon}.svg`;
 
   humidityValueDom.textContent = data.list[0].main.humidity;
   windSpeedValueDom.textContent = data.list[0].wind.speed;
-  visibilityValueDom.textContent = data.list[0].visibility;
+
+  const [sunriseTime, sunriseUnit] = formatAMPM(new Date(data.city.sunrise * 1000), lat, lng).split(' ');
+  const [sunsetTime, sunsetUnit] = formatAMPM(new Date(data.city.sunset * 1000), lat, lng).split(' ');
+  sunriseValueDom.textContent = sunriseTime;
+  sunsetValueDom.textContent = sunsetTime;
+  sunriseUnitDom.textContent = sunriseUnit;
+  sunsetUnitDom.textContent = sunsetUnit;
 
   //render hourly forecast
   for (let i = 0; i < 7; i++) {
@@ -72,4 +103,46 @@ export async function render(fetchFiveDayForecast, lat, lng, metric) {
     hourlyImgDom[i].src = `src/img/icons/${data.list[i].weather[0].icon}.svg`;
     hourlyTempDom[i].textContent = `${Math.round(data.list[i].main.temp)}°C`;
   }
+
+  let j = 0;
+  for (let i = 0; i < 40; i += 8) {
+    if (j === 0) fiveDayForecastDayDom[j].textContent = 'Today';
+    else fiveDayForecastDayDom[j].textContent = formatWeekDayName(new Date(data.list[i].dt * 1000), lat, lng);
+
+    fiveDayForecastImgDom[j].src = `src/img/icons/${data.list[i].weather[0].icon.replace('n', 'd')}.svg`; //remove 'n' from weather
+
+    const [maxTemp, minTemp] = findMinAndMaxTemp(i, data);
+    fiveDayForecastTempHigh[j].textContent = maxTemp;
+    fiveDayForecastTempLow[j].textContent = minTemp;
+
+    j++;
+  }
+}
+
+/**
+ * Go through data arr & find min & max temp for the day
+ * @param {*} startIdx
+ * @param {*} data
+ * @returns
+ */
+function findMinAndMaxTemp(startIdx, data) {
+  let maxTemp = -Infinity;
+  let minTemp = Infinity;
+
+  //loop through something for 8 times
+  for (let i = startIdx; i < startIdx + 8; i++) {
+    maxTemp = Math.max(maxTemp, data.list[i].main.temp_max);
+    minTemp = Math.min(minTemp, data.list[i].main.temp_min);
+  }
+
+  return [Math.round(maxTemp), Math.round(minTemp)];
+}
+
+/**
+ *
+ * @param {String} metricStr
+ */
+function convertMetrics(metricStr) {
+  if (metricStr === 'metric');
+  if (metricStr === 'imperial');
 }
